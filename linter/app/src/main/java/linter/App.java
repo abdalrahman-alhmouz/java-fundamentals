537227384e1e663cc4fc4e19cabaaf2f8b601c09
Abdalrahman jawad alhmouz <75928415+abdalrahman-alhmouz@users.noreply.github.com>
2021-04-09 20:47:16 +0300

    Update App.java

--- linter/app/src/main/java/linter/App.java
+++ linter/app/src/main/java/linter/App.java
@@ -20,28 +20,22 @@ public class App {
 
         System.out.println(new App().getGreeting());
 
-        Path path = Paths.get("C:\\Users\\Abood\\Abdalrahman\\401\\java-fundamentals\\linter\\app\\src\\main\\resources\\gates.js");
+        Path path = Paths.get("gates.js");
         try{
             List<String> lines=Files.readAllLines(path);
             System.out.println("the line test is " + lines);
             for(int i=0 ;i<lines.size();i++){
                 if (lines.get(i).contains(";")){
-//                    System.out.println("yes it have ;");
 
                 }else if (lines.get(i).contains("{")){
-//                    System.out.println("yes it {");
 
                 }else if (lines.get(i).contains("}")){
-//                    System.out.println("yes it }");
 
                 }else  if (lines.get(i).contains("if")){
-//                    System.out.println("yes it if");
 
                 }else if (lines.get(i).contains("else")){
-//                    System.out.println("yes it else");
 
                 }else if(lines.get(i).isEmpty()){
-//                    System.out.println("yes it null");
 
                 } else{
                     System.out.println("Line :"+(i+1)+" Missing semicolon");
